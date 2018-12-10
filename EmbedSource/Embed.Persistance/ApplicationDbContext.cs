80fe8974cc32d6d07f13d2fe510edde263ebfbdc
Philip Pang <pkcphilip@outlook.com>
2018-12-10 14:12:17 +0800

    Added EF6 to the Persistance layer. Created default ApplicationDbContext and added Product data set. Enabled migrations to the Persistance project.

---
+++ EmbedSource/Embed.Persistance/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Embed.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Embed.Persistance
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext() : base("EmbedDbContext")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ EmbedSource/Embed.Persistance/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using Embed.Core.Entities;
+
+namespace Embed.Persistance
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Product> Products { get; set; }
+    }
+}
