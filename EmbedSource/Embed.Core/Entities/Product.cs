dbbfe550b0b8a0cc23e20ee184e67c3d3ea342fe
Philip Pang <pkcphilip@outlook.com>
2018-12-10 14:04:01 +0800

    Added Product entity.

---
+++ EmbedSource/Embed.Core/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Embed.Core.Entities
+{
+    public class Product
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double SaleAmount { get; set; }
+
+        public void Modify(string name, int quantity, double saleAmount)
+        {
+            Name = name;
+            Quantity = quantity;
+            SaleAmount = saleAmount;
+        }
+    }
+}
